c75305be26628dfe108fd61b468ed75a155b54de
Victor <vsandru4@gmail.com>
2023-02-25 20:18:39 -0500

    finish a2

--- assignments/assignment2/main.cc
+++ assignments/assignment2/main.cc
@@ -1,29 +1,10 @@
-#include "PhotoGram.h"
+#include <iostream>
+#include "TestControl.h"
 
-int main(int argc, char const *argv[])
-{
-    // Date d1 = Date(2003, 1, 4);
-    // Photo p1 = Photo();
-    // Photo p2 = Photo("hello", d1, "conettt");
-    // Album a1 = Album("title", "desc");
-    // a1.addPhoto(new Photo(p2));
-    // a1.removePhoto(0);
+using namespace std;
 
-    Photo* ph1 = new Photo("title,", Date(), "con");
-    Photo* ph2 = new Photo("titl2", Date(), "ca");
-    Photo* ph3 = new Photo("ta3", Date(), "a");
-
-    Album* album = new Album("ti", "av");
-
-    album->addPhoto(0, ph1);
-    album->addPhoto(0, ph2);
-    album->addPhoto(1, ph3);
-    album->display();
-    // album->addPhoto(ph1);
-
-    delete album;
-    delete ph1;
-    delete ph2;
-    delete ph3;
+int main(){
+    TestControl control;
+    control.launch();
     return 0;
 }
